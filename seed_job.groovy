30627440ddd663480a653c1ed096cf213b36baff
nikhilbangar8 <40771758+nikhilbangar8@users.noreply.github.com>
2020-05-03 00:05:16 +0530

    Removed Triggers

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,18 +1,12 @@
 job('MyfirstJob') {
     
-    triggers {
-        scm('H/15 * * * *')
-    }
     steps {
         shell('echo First Job START')
     }
 }
 
 job('My-Second-Job') {
-    
-    triggers {
-        scm('H/15 * * * *')
-    }
+
     steps {
         shell('echo second Job START')
     }
