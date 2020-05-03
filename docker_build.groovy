cc69d705efa1a283aca1b725d3fa137e719160af
nikhilbangar8 <40771758+nikhilbangar8@users.noreply.github.com>
2020-05-03 20:46:33 +0530

    Adding Pipeline script

--- docker_build.groovy
+++ docker_build.groovy
@@ -1,6 +1,35 @@
-job('Docker Build Job') {
-  
-    steps {
-        shell('sudo docker --version')
+pipelineJob('Docker Build Job') {
+  definition {
+    cps {
+      script('''
+          pipeline {
+          agent any
+                stages {
+                    stage('Checking Docker Version') {
+                        steps {
+                            sudo docker --version
+                        }
+                    }
+                    stage('Starting Linux image in Docker') {
+                        steps {
+                            echo 'logic'
+                            echo pwd
+                        }
+                    }
+                    stage('Build Docker Image for Tomcat Server') {
+                        steps {
+                            echo 'logic'
+                        }
+                    }
+                    stage('End Of the Stage') {
+                        steps {
+                            echo 'logic'
+                        }
+                    }
+                }
+            }
+        ''')
+      sandbox()
     }
+  }
 }
