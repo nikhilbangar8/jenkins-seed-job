ebe43084c317837baca95f6f7c1fb4ff89cb4a39
nikhilbangar8 <40771758+nikhilbangar8@users.noreply.github.com>
2020-05-05 17:20:22 +0530

    Update container_clean_up.groovy

--- container_clean_up.groovy
+++ container_clean_up.groovy
@@ -9,14 +9,14 @@ pipelineJob('Container Cleanup') {
                         steps {
                                 echo "Stopping all Containers"
                                 sh "sudo docker ps -a -q"
-                                sh """ sudo docker stop $(docker ps -a -q) """
+                                sh ''' sudo docker stop $(docker ps -a -q) '''
                         }
                         
                     }
                     stage('Removing all Containers') {
                         steps {
                                 echo "Removing all Containers"
-                                sh "sudo docker rm {$('docker ps -a -q')}"
+                                sh '''sudo docker rm $(docker ps -a -q) '''
                         }
                         
                     }  
