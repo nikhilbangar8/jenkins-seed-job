ee281756ec2a9130a2b17cecd04d2b57cfaabee6
nikhilbangar8 <40771758+nikhilbangar8@users.noreply.github.com>
2020-12-22 02:34:34 +0530

    Update django_app_build.groovy

--- django_app_build.groovy
+++ django_app_build.groovy
@@ -45,7 +45,7 @@ pipelineJob('RnDX Django App Build') {
                         steps {
                             sh "sudo docker images"
                             //sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 211448159615.dkr.ecr.us-east-1.amazonaws.com"
-                            sh "docker tag ${dockerimagename}:${app_version} ${ECR_Repo}:${app_version}"
+                            sh "sudo docker tag ${dockerimagename}:${app_version} ${ECR_Repo}:${app_version}"
                             echo "Image Uploaded to ECR Repo"
                         }
                     }
